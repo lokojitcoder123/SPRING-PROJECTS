f323648aec1d822b6ea2adfb31b27f279a5d2936
Lokojit Biswas <lokojitb@gmail.com>
2026-03-27 21:48:16 +0530

    Add files via upload

---
+++ SpringCore-project-02/src/main/java/org/cfs/DiselEngine.java
@@ -0,0 +1,11 @@
+package org.cfs;
+
+public class DiselEngine  implements Engine{
+    public DiselEngine() {
+        System.out.println("car default constructor");
+    }
+
+    public int start(){
+        return 1;
+    }
+}
